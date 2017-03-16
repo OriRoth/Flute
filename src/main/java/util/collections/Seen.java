72b919a802fb552dabd04e25b89ccb02dba37a65
Ori Roth <ori.rothh@gmail.com>
2017-03-16 20:07:47 +0200

    seen addition

--- src/main/java/util/collections/Seen.java
+++ src/main/java/util/collections/Seen.java
@@ -4,6 +4,10 @@ import java.util.Collection;
 import java.util.HashSet;
 import java.util.function.Consumer;
 
+/**
+ * @author Ori Roth
+ * @since Mar 16, 2017
+ */
 public class Seen<T> extends HashSet<T> {
   private static final long serialVersionUID = -1578854262738867470L;
 
@@ -19,6 +23,11 @@ public class Seen<T> extends HashSet<T> {
       return true;
     addAll(collection);
     return false;
+  }
+
+  public void ifSeen(T t, Consumer<T> operation) {
+    if (seen(t))
+      operation.accept(t);
   }
 
   public void ifNotSeen(T t, Consumer<T> operation) {
