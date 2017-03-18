46cba1c60d5228f57fd8c40807e1f81b9ebafefd
Ori Roth <ori.rothh@gmail.com>
2017-03-18 13:17:57 +0200

    documenting

--- src/main/java/util/Initializable.java
+++ src/main/java/util/Initializable.java
@@ -5,19 +5,45 @@ import java.util.function.Supplier;
 
 import static util.Test.*;
 
+/**
+ * A lazy wrapper for object.
+ * 
+ * @author Ori Roth
+ * @since Mar 18, 2017
+ */
 public class Initializable<T> {
   private T value;
   private boolean initialized;
 
+  /**
+   * @return an empty initializable
+   * @see #suggest
+   * @see #get
+   */
   public static <T> Initializable<T> initializable() {
     return new Initializable<>();
   }
 
+  /**
+   * @param t
+   *          initial object
+   * @return initialized initializable
+   */
   public static <T> Initializable<T> initializable(T t) {
     return new Initializable<T>().set(t);
   }
 
-  public Initializable<T> set(T value) {
+  /**
+   * Set the wrapped object in this initializable. Will work only if this
+   * initializable is not yet initialized.
+   * 
+   * @param value
+   *          initial value for this initializable
+   * @return this initializable
+   * @throws IllegalStateException
+   *           if this initializable is already initialized
+   */
+  protected Initializable<T> set(T value) {
     if (initialized)
       throw new IllegalStateException();
     initialized = true;
@@ -25,14 +51,28 @@ public class Initializable<T> {
     return this;
   }
 
+  /**
+   * @return wrapped object or null if this initializable is not yet initialized
+   */
   public T get() {
     return value;
   }
 
+  /**
+   * @return true iff this initializable is initialized
+   */
   public boolean initialized() {
     return initialized;
   }
 
+  /**
+   * Suggests an initialization for this initializable. If the initializable is
+   * already initialized, nothing happens.
+   * 
+   * @param supplier
+   *          supplier of initial object for this initializable
+   * @return the wrapped object in this initializable
+   */
   public T suggest(Supplier<T> supplier) {
     return initialized() ? get() : set(supplier.get()).get();
   }
@@ -47,12 +87,12 @@ public class Initializable<T> {
     return obj instanceof Initializable ? equalsNullable(value, ((Initializable<?>) obj).value)
         : equalsNullable(value, obj);
   }
-  
+
   @Override
   public int hashCode() {
     return Objects.hashCode(value);
   }
-  
+
   @Override
   protected Object clone() {
     return initializable(value);
--- src/main/java/util/Lazy.java
+++ src/main/java/util/Lazy.java
@@ -1,5 +1,6 @@
 package util;
 
+import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
@@ -9,8 +10,25 @@ import util.fluent.collections.FluentMap;
 
 import static util.Initializable.*;
 
+/**
+ * Lazy wrappers fpr functional interfaces.
+ * 
+ * @author Ori Roth
+ * @since Mar 18, 2017
+ */
 public class Lazy {
+  /**
+   * A lazy wrapper for a {@link Supplier}. This {@link Supplier} would record
+   * the first value the input supplier had supplied, then return it every time
+   * on {@link Supplier#get}. The value is calculated on the first
+   * {@link Supplier#get}.
+   * 
+   * @param supplier
+   *          a supplier to wrap
+   * @return a lazy version of this supplier
+   */
   public static <T> Supplier<T> lazy(Supplier<T> supplier) {
+    Objects.requireNonNull(supplier);
     return new Supplier<T>() {
       Initializable<T> value = initializable();
 
@@ -20,7 +38,17 @@ public class Lazy {
     };
   }
 
+  /**
+   * A lazy wrapper for a {@link Consumer}. If it {@link Consumer#accept}s an
+   * object it already consumed, it will do nothing- else activating original
+   * consumer.
+   * 
+   * @param consumer
+   *          a consumer to wrap
+   * @return a lazy version of this consumer
+   */
   public static <T> Consumer<T> lazy(Consumer<T> consumer) {
+    Objects.requireNonNull(consumer);
     return new Consumer<T>() {
       Seen<T> seen = new Seen<>();
 
@@ -31,7 +59,16 @@ public class Lazy {
     };
   }
 
+  /**
+   * A lazy wrapper for a {@link Function}. If it gets an input it already
+   * calculated, it would return the previous calculation.
+   * 
+   * @param function
+   *          a function to wrap
+   * @return a lazy version of this function
+   */
   public static <T, R> Function<T, R> lazy(Function<T, R> function) {
+    Objects.requireNonNull(function);
     return new Function<T, R>() {
       FluentMap<T, R> results = new FluentMap<>();
 
