35577aea34bb3ee034e8f619d34fd205b26a04a4
Ori Roth <ori.rothh@gmail.com>
2017-03-19 20:41:10 +0200

    tree stuff

--- src/main/java/util/trees/FixedTree.java
+++ src/main/java/util/trees/FixedTree.java
@@ -1,14 +1,83 @@
 package util.trees;
 
 import java.util.ArrayList;
+import java.util.Collection;
 
-// TODO Roth: complete
+import util.FluentAPI;
+
+/**
+ * Fixed size tree, i.e. there is a limited number of children allowed in this
+ * tree at all time.
+ * 
+ * @author Ori Roth
+ * @since Mar 19, 2017
+ * @param <T>
+ *          JD
+ */
 public class FixedTree<T> extends ArrayList<Tree<T>> implements Tree<T> {
   private static final long serialVersionUID = -5603703432180427358L;
+  private T value;
+  private int limit;
+
+  /**
+   * @param value
+   *          node value
+   * @param limit
+   *          children limit
+   */
+  public FixedTree(T value, int limit) {
+    this.value = value;
+    this.limit = limit;
+  }
+
+  @Override
+  public void add(int index, Tree<T> element) {
+    if (size() > limit - 1)
+      throw new IllegalStateException();
+    super.add(index, element);
+  }
+
+  @Override
+  public boolean add(Tree<T> e) {
+    if (size() > limit - 1)
+      throw new IllegalStateException();
+    return super.add(e);
+  }
+
+  @Override
+  public boolean addAll(Collection<? extends Tree<T>> c) {
+    if (c != null && size() > limit - c.size())
+      throw new IllegalStateException();
+    return super.addAll(c);
+  }
+
+  @Override
+  public boolean addAll(int index, Collection<? extends Tree<T>> c) {
+    if (c != null && size() > limit - c.size())
+      throw new IllegalStateException();
+    return super.addAll(index, c);
+  }
 
   @Override
   public T value() {
-    // TODO Auto-generated method stub
-    return null;
+    return value;
+  }
+
+  /**
+   * @return children limit of this tree
+   */
+  public int limit() {
+    return limit;
+  }
+
+  /**
+   * @param limit
+   *          new children limit
+   * @return this tree
+   */
+  @FluentAPI
+  public FixedTree<T> limit(int limit) {
+    this.limit = limit;
+    return this;
   }
 }
--- src/main/java/util/trees/Tree.java
+++ src/main/java/util/trees/Tree.java
@@ -4,6 +4,7 @@ import java.util.Collection;
 import java.util.LinkedList;
 import java.util.List;
 
+// TODO Roth: add default algorithms
 /**
  * A tree interface. In this interface, each node is a tree as well.
  * 
@@ -13,8 +14,17 @@ import java.util.List;
  *          tree node value type
  */
 public interface Tree<T> extends List<Tree<T>> {
+  /**
+   * @return node value
+   */
   public T value();
 
+  /**
+   * @param t
+   *          node value
+   * @return a leaf, i.e a tree inintialized with input value that cannot be
+   *         added children
+   */
   public static <T> Tree<T> leaf(T t) {
     abstract class _Tree<X> extends LinkedList<Tree<X>> implements Tree<X> {
       private static final long serialVersionUID = 8344439290938922855L;
@@ -26,6 +36,26 @@ public interface Tree<T> extends List<Tree<T>> {
 
       @Override
       public boolean addAll(Collection<? extends Tree<X>> c) {
+        throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public boolean add(Tree<X> e) {
+        throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public boolean addAll(int index, Collection<? extends Tree<X>> c) {
+        throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public void addFirst(Tree<X> e) {
+        throw new UnsupportedOperationException();
+      }
+
+      @Override
+      public void addLast(Tree<X> e) {
         throw new UnsupportedOperationException();
       }
     }
